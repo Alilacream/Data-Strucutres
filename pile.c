ba714b85e3085eca0b6e616b1d7c90dd2167c773
Alilacream <amaramohammedali42@gmail.com>
2025-10-20 10:42:01 +0100

    added a perfect running gcc shell script

--- pile.c
+++ pile.c
@@ -3,20 +3,15 @@
 #include <stdio.h>
 int main(int argc, char *argv[]) {
   Pile *pile = (Pile *)malloc(sizeof(Pile));
-
-  pile->Sommet->id = 1;
-  pile->Sommet->next = NULL;
-
+  // PUSH to stack.
+  empilerPile(pile, 1);
   empilerPile(pile, 2);
   empilerPile(pile, 3);
 
   bool empty = estVide(pile);
-  if (empty) {
+  if (empty)
     printf("the list is empty");
-  }
-
   depilerPile(pile);
-
   printStack(pile);
 
   return 0;
